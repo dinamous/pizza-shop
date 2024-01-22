7507eb22b17088890974b9fbc45beee361172704
Matheus Simões <matheusaugustosimoes@hotmail.com>
2024-01-22 17:43:01 -0300

    feat: :sparkles: Forms do singn-in

--- src/pages/auth/SignIn.vue
+++ src/pages/auth/SignIn.vue
@@ -1,11 +1,54 @@
 <script setup lang="ts">
+
+import useValidade from '@vuelidate/core'
+import { email, required } from '@vuelidate/validators'
+import { reactive, ref } from 'vue'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+
+const isSubmitting = ref(false);
+
+const formData = reactive({
+  email: ''
+})
+
+const rules = {
+  email: { required, email },
+}
+
+const v$ = useValidade(rules, formData)
+
+const submitForm = async () => {
+
+  try {
+    // Lógica de submissão aqui (por exemplo, chamada de API)
+    // const result = await v$.value.$validate()
+    console.log(formData.email);
+
+    // Simulação de uma chamada assíncrona
+    isSubmitting.value = true;
+    // const result = await v$.value.$validate()
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    // Lógica após a submissão bem-sucedida
+
+  } catch (error) {
+    // Lógica em caso de falha na submissão
+
+  } finally {
+    isSubmitting.value = false;
+  }
+}
+
+
 </script>
 
 <template lang="">
-  <div class="p-8">
+  <div
+    class="p-8"
+  >
     <div class="w-[350px] flex - flex-col justify-center gap-6 ">
       <div class="flex flex-col gap-2 text-center">
         <h1 class="txt-2xl font-semibold tracking-tight">
@@ -15,16 +58,22 @@ import { Label } from '@/components/ui/label'
           Acompanhe suas vendas pelo painel do parceiro.
         </p>
       </div>
-      <form  class="space-y-4">
+      <form
+        @submit.prevent="submitForm"
+        class="space-y-4"
+      >
         <div class="space-y-2">
           <Label for="email">Seu email</Label>
           <Input
             id="email"
             type="email"
-            placeholder="aaaaaaaaa"
+            v-model="formData.email"
           />
         </div>
-        <Button class="w-full">
+        <Button
+          class="w-full"
+          :disabled="isSubmitting"
+        >
           Acessar painel
         </Button>
       </form>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
-
-import tailwind from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import path from 'path';
+import tailwind from 'tailwindcss';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
